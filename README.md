49f16e36b2d6fa07fdad2728ecf0b51d0a73fab1
Claudio Sanches <contato@claudiosanches.com>
2017-10-27 22:14:28 -0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# wp-git-hooks
-Git hooks for WordPress plugins and themes development
+# WP GIT Hooks
+
+GIT hooks for WordPress plugins and themes development.
+
+## Hooks
+
+* `pre-commit` - Lint and check coding standards using [WPCS](https://packagist.org/packages/wp-coding-standards/wpcs).
+
+## Installation
+
+```bash
+composer require claudiosanches/wp-git-hooks
+```
+
+## Setup
+
+Include the follow lines into the project's `composer.json`:
+
+```
+"scripts": {
+    "pre-update-cmd": [
+        "ClaudioSanches\\WpGitHooks\\Hooks::preHooks"
+    ],
+    "pre-install-cmd": [
+        "ClaudioSanches\\WpGitHooks\\Hooks::preHooks"
+    ],
+    "post-install-cmd": [
+        "\"vendor/bin/phpcs\" --config-set installed_paths vendor/wp-coding-standards/wpcs",
+        "ClaudioSanches\\WpGitHooks\\Hooks::postHooks"
+    ],
+    "post-update-cmd": [
+        "\"vendor/bin/phpcs\" --config-set installed_paths vendor/wp-coding-standards/wpcs",
+        "ClaudioSanches\\WpGitHooks\\Hooks::postHooks"
+    ]
+}
+```
+
+This will install all hooks and setup WPCS.
+
+### Configuration file
+
+Supports a configured file named `.wp-git-hooks.yml`.
+
+See an example of configuration file to make PHPCS/WPCS ignore files and paths:
+
+```yml
+pre_commit:
+  ignore:
+    - foo.php
+    - includes/bar.php
+    - test/
+```
+
+### Sample phpcs.ruleset.xml for PHPCS/WPCS
+
+A `phpcs.ruleset.xml` file is required in order to correct run your project's coding standards.
+
+```xml
+<?xml version="1.0"?>
+<ruleset name="WordPress Coding Standards">
+    <description>PHP_CodeSniffer ruleset for WordPress plugins and themes development.</description>
+
+    <rule ref="WordPress"></rule>
+</ruleset>
+```
+
+### Manual setup (optional)
+
+By default should already run all commands from composer `scripts` when installed, but if necessary run it manually is possible with the follow commands:
+
+```bash
+composer run-script pre-update-cmd
+composer run-script post-update-cmd
+```
+
+## Release history
+
+- 2017-10-27 - 1.0.0 - Initial release.
+
+## Sources
+
+Inspired by:
+
+- <https://github.com/juizmill/git-hooks>
+- <https://github.com/smgladkovskiy/phpcs-git-pre-commit>
---
+++ src/Hooks.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Hooks
+ *
+ * Allow install or uninstall WP GIT Hooks.
+ *
+ * PHP Version 7
+ *
+ * @category Hooks
+ * @package  ClaudioSanches\WpGitHooks
+ * @author   Claudio Sanches <contato@claudiosanches.com>
+ * @license  https://git.io/vFkiA GPLv3
+ * @link     https://github.com/claudiosanches/wp-git-hooks
+ */
+
+namespace ClaudioSanches\WpGitHooks;
+
+use Exception;
+use Composer\Script\Event;
+
+/**
+ * Hooks class.
+ *
+ * @category Hooks
+ * @package  ClaudioSanches/WpGitHooks
+ * @author   Claudio Sanches <contato@claudiosanches.com>
+ * @license  https://git.io/vFkiA GPLv3
+ * @link     https://github.com/claudiosanches/wp-git-hooks
+ */
+class Hooks
+{
+
+    /**
+     * Hooks.
+     *
+     * @var string[]
+     */
+    protected static $hooks = [
+        'pre-commit',
+    ];
+
+    /**
+     * Get current directory.
+     *
+     * @param Event $event Composer event.
+     *
+     * @return string
+     */
+    protected static function getProjectDir(Event $event)
+    {
+        return $event->getComposer()->getConfig()->get('vendor-dir')
+            . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * Pre hooks.
+     *
+     * @param Event $event Composer event.
+     *
+     * @return bool
+     */
+    public static function preHooks(Event $event)
+    {
+
+        $directory = self::getProjectDir($event);
+        $io        = $event->getIO();
+        $removed   = false;
+
+        foreach (self::$hooks as $hookName) {
+            $hook = sprintf(
+                '%1$s.git%3$shooks%3$s%2$s',
+                $directory,
+                $hookName,
+                DIRECTORY_SEPARATOR
+            );
+
+            if (file_exists($hook)) {
+                unlink($hook);
+                $removed = true;
+            }
+        }
+
+        if ($removed) {
+            $io->write('<info>Hooks removed!</info>');
+        }
+
+        return true;
+    }
+
+    /**
+     * Post hooks.
+     *
+     * @param Event $event Composer event.
+     *
+     * @return bool
+     */
+    public static function postHooks(Event $event)
+    {
+        $directory = self::getProjectDir($event);
+
+        if (!file_exists($directory . '.git')) {
+            throw new Exception(sprintf('Opss! Local Git repository not found.'));
+        }
+
+        $io = $event->getIO();
+
+        foreach (self::$hooks as $hookName) {
+            $hook     = sprintf(
+                '%1$s.git%3$shooks%3$s%2$s',
+                $directory,
+                $hookName,
+                DIRECTORY_SEPARATOR
+            );
+            $original = sprintf(
+                '%1$s%3$s..%3$shooks%3$s%2$s',
+                __DIR__,
+                $hookName,
+                DIRECTORY_SEPARATOR
+            );
+
+            copy($original, $hook);
+            chmod($hook, 0777);
+        }
+
+        $io->write('<info>Hooks added!</info>');
+
+        return true;
+    }
+}
